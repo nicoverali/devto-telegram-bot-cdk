640bfe56685e979bfd7fb1f886a79ba41af15028
Nicolas A. Vera <nicolasalevera98@gmail.com>
2024-08-24 20:49:21 -0300

    Initial commit

---
+++ test/dev.to-bot-post.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DevToBotPost from '../lib/dev.to-bot-post-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dev.to-bot-post-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DevToBotPost.DevToBotPostStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
